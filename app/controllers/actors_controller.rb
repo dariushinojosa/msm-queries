7faa6e39dbab7afb61bf09291d711821857cb20d
dariushinojosa <101020189+dariushinojosa@users.noreply.github.com>
2024-11-05 22:57:46 +0000

    hand in

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,10 +1,14 @@
 class ActorsController < ApplicationController
 
   def all
+    @actors_list = Actor.all
     render({ :template => "actors_templates/all"})
   end
 
   def show
+    the_id = params.fetch("the_id")
+    @this_act = Actor.where({ :id => the_id }).first
+    @matching_chars = Character.where({ :actor_id => the_id })
     render({ :template => "actors_templates/show"})
   end
 
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,13 @@
 class MoviesController < ApplicationController
 
   def all
+    @movies_list = Movie.all
     render({ :template => "movies_templates/all"})
   end
 
   def show
+    the_id = params.fetch("the_id")
+    @this_mov = Movie.where({ :id => the_id }).first
     render({ :template => "movies_templates/show"})
   end
 
